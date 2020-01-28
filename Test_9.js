6b8c9e1e2ecf70d2e6ca16a81087e68c3340c192
Emin91 <emin.proger@gmail.com>
2020-01-28 11:58:01 +0400

    test

--- Test_9.js
+++ Test_9.js
@@ -1,10 +1,6 @@
-//While, квадрат числа
-
-var count = 0
-var a = 0
-
-while (count < 10 && a < 10) {
-    count++
-    a++
-    document.write(`${a} * ${a} = ${a*a} </br> `)
-}
+hfbdvfdvdxki.[
+    ';mj
+    , kh
+    '.,kh
+lk, kh., khmjgnh''
+]
